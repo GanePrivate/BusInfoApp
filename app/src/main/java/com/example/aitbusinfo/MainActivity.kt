c5750c888fe8da661ba0e0aceac180a2e14a67ee
gane0528 <k17028kk@aitech.ac.jp>
2021-02-21 20:56:37 +0900

    少しコードを見やすくした？

--- app/src/main/java/com/example/aitbusinfo/MainActivity.kt
+++ app/src/main/java/com/example/aitbusinfo/MainActivity.kt
@@ -21,27 +21,32 @@ class MainActivity : AppCompatActivity() {
         val busInfo= getAllData()
 
         // 今日バスの運行があるか確認する
-        if (busInfo.todayInfo.daiya == null) {
-            nextTime.text = "本日、バスの運行は\nありません"
-            afterNextTime.visibility = View.INVISIBLE
-            daiya.visibility = View.INVISIBLE
-            label2.visibility = View.INVISIBLE
-            label3.visibility = View.INVISIBLE
-        }else{
-            // 次の出発時刻を表示する
-            if (busInfo.toDaigakuInfo.minutes != -1) {
-                nextTime.text = "%02d:%02d".format(busInfo.toDaigakuInfo.hour, busInfo.toDaigakuInfo.minutes)
-            } else {
-                nextTime.text = if (LocalTime.now().minute <= 7) "[始発] 08:%02d".format(busInfo.todayInfo.toDaigakuFirst) else "本日の運行は終了しました"
+        when (busInfo.todayInfo.daiya) {
+            null -> {
+                nextTime.text = "本日、バスの運行は\nありません"
+                afterNextTime.visibility = View.INVISIBLE
+                daiya.visibility = View.INVISIBLE
+                label2.visibility = View.INVISIBLE
+                label3.visibility = View.INVISIBLE
             }
+            else -> {
+                // 次の出発時刻を表示する
+                if (busInfo.toDaigakuInfo.minutes != -1) {
+                    nextTime.text = "%02d:%02d".format(busInfo.toDaigakuInfo.hour, busInfo.toDaigakuInfo.minutes)
+                } else {
+                    nextTime.text = if (LocalTime.now().minute <= 7) "[始発] 08:%02d".format(busInfo.todayInfo.toDaigakuFirst) else "本日の運行は終了しました"
+                }
 
-            // 次の次の出発時間を表示する
-            if (busInfo.toDaigakuAfterNext.minutes != -1) {
-                afterNextTime.text = "%02d:%02d".format(busInfo.toDaigakuAfterNext.hour, busInfo.toDaigakuAfterNext.minutes)
+                // 次の次の出発時間を表示する
+                if (busInfo.toDaigakuAfterNext.minutes != -1) {
+                    afterNextTime.text = "%02d:%02d".format(busInfo.toDaigakuAfterNext.hour, busInfo.toDaigakuAfterNext.minutes)
+                } else{
+                    afterNextTime.text = "この時間の\nバスはありません"
+                }
+
+                // 今日の運行ダイヤを表示する
+                daiya.text = "今日は${busInfo.todayInfo.daiya}ダイヤです"
             }
-
-            // 今日の運行ダイヤを表示する
-            daiya.text = "今日は${busInfo.todayInfo.daiya}ダイヤです"
         }
 
         // OnTabSelectedListenerの実装
@@ -71,9 +76,7 @@ class MainActivity : AppCompatActivity() {
                         }
 
                         // 次の次の出発時間を表示する
-                        if (busInfo.toYakusaAfterNext.minutes != -1) {
-                            afterNextTime.text = "%02d:%02d".format(busInfo.toYakusaAfterNext.hour, busInfo.toYakusaAfterNext.minutes)
-                        }
+                        afterNextTime.text = if (busInfo.toYakusaAfterNext.minutes != -1) "%02d:%02d".format(busInfo.toYakusaAfterNext.hour, busInfo.toYakusaAfterNext.minutes) else "この時間の\nバスはありません"
 
                         // 今日の運行ダイヤを表示する
                         daiya.text = "今日は${busInfo.todayInfo.daiya}ダイヤです"
@@ -95,9 +98,7 @@ class MainActivity : AppCompatActivity() {
                         }
 
                         // 次の次の出発時間を表示する
-                        if (busInfo.toDaigakuAfterNext.minutes != -1) {
-                            afterNextTime.text = "%02d:%02d".format(busInfo.toDaigakuAfterNext.hour, busInfo.toDaigakuAfterNext.minutes)
-                        }
+                        afterNextTime.text = if (busInfo.toDaigakuAfterNext.minutes != -1) "%02d:%02d".format(busInfo.toDaigakuAfterNext.hour, busInfo.toDaigakuAfterNext.minutes) else "この時間の\nバスはありません"
 
                         // 今日の運行ダイヤを表示する
                         daiya.text = "今日は${busInfo.todayInfo.daiya}ダイヤです"
